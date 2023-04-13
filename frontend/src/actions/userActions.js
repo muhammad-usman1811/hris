2a2d823338ee44a8baf3d49b01defb6d3c3b4442
Muhammad Usman <musmandev.pk@gmail.com>
2023-04-13 10:03:35 +0500

    Update userActions.js

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -12,7 +12,7 @@ export const login = (email, password) => async (dispatch) => {
       },
     };
     const { data } = await axios.post(
-      "http://localhost:5000/api/users/login",
+      "/api/users/login",
       { email, password },
       config
     );
