497f5147470120c6ea7b6e1379034e6c61818a90
Muhammad Usman <musmandev.pk@gmail.com>
2023-05-08 16:14:53 +0500

    make leave card dynamic

--- frontend/src/components/LeaveRequestCard.jsx
+++ frontend/src/components/LeaveRequestCard.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import CircularProgress from "@mui/material/CircularProgress";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -12,93 +14,137 @@ import Chip from "@mui/material/Chip";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import { getLeaves } from "../actions/leaveActions";
 
 const LeaveRequestCard = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  const leavesList = useSelector((state) => state.leavesList);
+  const { loading, leaves } = leavesList;
+
+  const recentLeaves = leaves?.slice(-2);
+  const firstLeave = recentLeaves && recentLeaves[1];
+  const secondLeave = recentLeaves && recentLeaves[0];
+
+  useEffect(() => {
+    dispatch(getLeaves());
+  }, [dispatch]);
   return (
-    <Card
-      sx={{
-        borderRadius: "8px",
-        width: "400px",
-      }}
-    >
-      <CardContent>
-        <Typography variant="h6">Leave Requests</Typography>
-        <List
+    <>
+      {loading ? (
+        <Box
           sx={{
-            width: "100%",
-            maxWidth: 400,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          <Box
-            sx={{
-              bgcolor: "#E7EBEE",
-              marginBottom: "10px",
-              borderRadius: "8px",
-            }}
-          >
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar
-                  alt="Michael"
-                  src="https://mui.com/static/images/avatar/3.jpg"
-                />
-              </ListItemAvatar>
-              <ListItemText
-                primary="Michael"
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      sx={{ display: "inline" }}
-                      component="span"
-                      variant="body2"
-                      color="text.primary"
-                    >
-                      Urgent Leave
-                    </Typography>
-                    {" — I'll be in your neighborhood doing errands…"}
-                  </React.Fragment>
-                }
-              />
-              <Chip label="Pending" color="error" variant="outlined" />
-            </ListItem>
-          </Box>
-          <Box sx={{ bgcolor: "#E7EBEE", borderRadius: "8px" }}>
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar
-                  alt="Mark Taylor"
-                  src="https://mui.com/static/images/avatar/2.jpg"
-                />
-              </ListItemAvatar>
-              <ListItemText
-                primary="Mark Taylor"
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      sx={{ display: "inline" }}
-                      component="span"
-                      variant="body2"
-                      color="text.primary"
-                    >
-                      Casual Leave
-                    </Typography>
-                    {" — Wish I could come, but I'm out of town this…"}
-                  </React.Fragment>
-                }
-              />
-              <Chip label="Pending" color="error" variant="outlined" />
-            </ListItem>
-          </Box>
-        </List>
-      </CardContent>
-      <CardActions>
-        <Button size="small" onClick={() => navigate("/home/leaves")}>
-          Details
-        </Button>
-      </CardActions>
-    </Card>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Card
+          sx={{
+            borderRadius: "8px",
+            width: "400px",
+          }}
+        >
+          <CardContent>
+            <Typography variant="h6">Leave Requests</Typography>
+            <List
+              sx={{
+                width: "100%",
+                maxWidth: 400,
+              }}
+            >
+              <Box
+                sx={{
+                  bgcolor: "#E7EBEE",
+                  marginBottom: "10px",
+                  borderRadius: "8px",
+                }}
+              >
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar
+                      alt="Michael"
+                      src="/images/userPhoto.jpg"
+                      //src="https://mui.com/static/images/avatar/3.jpg"
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={firstLeave && firstLeave.name}
+                    secondary={
+                      <React.Fragment>
+                        <Typography
+                          sx={{ display: "inline" }}
+                          component="span"
+                          variant="body2"
+                          color="text.primary"
+                        >
+                          {firstLeave && firstLeave.type}
+                        </Typography>
+                        {` — ${firstLeave && firstLeave.reason}`}
+                      </React.Fragment>
+                    }
+                  />
+                  <Chip
+                    label={firstLeave && firstLeave.status}
+                    color={
+                      firstLeave && firstLeave.status === "Approved"
+                        ? "success"
+                        : "error"
+                    }
+                    variant="outlined"
+                  />
+                </ListItem>
+              </Box>
+              <Box sx={{ bgcolor: "#E7EBEE", borderRadius: "8px" }}>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar
+                      alt="Mark Taylor"
+                      src="/images/userPhoto.jpg"
+                      //src="https://mui.com/static/images/avatar/2.jpg"
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={secondLeave && secondLeave.name}
+                    secondary={
+                      <React.Fragment>
+                        <Typography
+                          sx={{ display: "inline" }}
+                          component="span"
+                          variant="body2"
+                          color="text.primary"
+                        >
+                          {secondLeave && secondLeave.type}
+                        </Typography>
+                        {` — ${secondLeave && secondLeave.reason}`}
+                      </React.Fragment>
+                    }
+                  />
+                  <Chip
+                    label={secondLeave && secondLeave.status}
+                    color={
+                      secondLeave && secondLeave.status === "Approved"
+                        ? "success"
+                        : "error"
+                    }
+                    variant="outlined"
+                  />
+                </ListItem>
+              </Box>
+            </List>
+          </CardContent>
+          <CardActions>
+            <Button size="small" onClick={() => navigate("/home/leaves")}>
+              Details
+            </Button>
+          </CardActions>
+        </Card>
+      )}
+    </>
   );
 };
 
