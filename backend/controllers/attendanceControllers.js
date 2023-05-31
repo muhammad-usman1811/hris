7254e80aef4cacc86be40725cf7f4ba0e7865266
Muhammad Usman <musmandev.pk@gmail.com>
2023-05-31 13:37:56 +0500

    adding wait in sending emails

--- backend/controllers/attendanceControllers.js
+++ backend/controllers/attendanceControllers.js
@@ -100,8 +100,22 @@ const sendEmailForCheckIn = async (req, res) => {
       },
     ]);
 
-    if (usersMissingAttendance) {
-      usersMissingAttendance.forEach(async (user) => {
+    if (usersMissingAttendance.length > 0) {
+      //Create email configuration
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+
+        port: 465,
+
+        secure: true,
+
+        auth: {
+          user: "hrisdigifloat@gmail.com",
+          pass: "qjvuxxgqkhakreax",
+        },
+      });
+
+      for (const user of usersMissingAttendance) {
         // Access the properties of each user
         const name = user.name;
         const email = user.email;
@@ -116,21 +130,7 @@ const sendEmailForCheckIn = async (req, res) => {
     <p style="font-weight: bold;"><span style="color:red">Digi</span>float (Private) Ltd.</p>
   `;
 
-        // Create and send the email using a suitable email sending library (e.g., Nodemailer)
-        const transporter = nodemailer.createTransport({
-          host: "smtp.gmail.com",
-
-          port: 465,
-
-          secure: true,
-
-          auth: {
-            user: "hrisdigifloat@gmail.com",
-            pass: "qjvuxxgqkhakreax",
-          },
-        });
-
-        transporter.sendMail({
+        await transporter.sendMail({
           from: "HRIS <hrisdigifloat@gmail.com>",
 
           to: email,
@@ -139,7 +139,7 @@ const sendEmailForCheckIn = async (req, res) => {
 
           html: html,
         });
-      });
+      }
     }
   } catch (error) {
     console.error(error);
